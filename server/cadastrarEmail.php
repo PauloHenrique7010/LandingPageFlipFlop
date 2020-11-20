3e9f69da088d5d07c049f953fdb0ee9b672bf38b
Paulo Henrique <paulohenrique7010@gmail.com>
2020-11-20 18:50:57 -0300

    Ajustes finais

--- server/cadastrarEmail.php
+++ server/cadastrarEmail.php
@@ -11,23 +11,30 @@ $json = json_decode($json);
 $email = $json->email;
 
 $resposta = array();
-//valida e já cadastra no banco
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    if (cadastrarEmail($email)) {
-        http_response_code(201);
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) { //é um email valido
+    if (verificaEmailExiste($email)) { //se o email informado ja esta no banco
+        http_response_code(203);
+        $resposta = [
+            'title' => 'Ops!',
+            'message' => 'O e-mail informado já está cadastrado no banco de dados!'
+        ];
+    } else { //nao existe email no BD
+        if (cadastrarEmail($email)) { //Consegiu cadastrar
+            http_response_code(201);
 
-        $resposta = [
-            'title' => 'Sucesso!',
-            'message' => 'E-mail cadastrado em nosso banco de dados!'
-        ];
-    } else {
-        http_response_code(202);
-        $resposta = [
-            'title' => 'Erro!',
-            'message' => $conexao->error
-        ];
+            $resposta = [
+                'title' => 'Sucesso!',
+                'message' => 'E-mail cadastrado em nosso banco de dados!'
+            ];
+        } else { //nao consegiu cadastrar
+            http_response_code(202);
+            $resposta = [
+                'title' => 'Erro!',
+                'message' => $conexao->error
+            ];
+        }
     }
-} else {
+} else { //email nao e valido
     http_response_code(203);
     $resposta = [
         'title' => 'Ops...',
@@ -35,11 +42,31 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     ];
 }
 
+
 //devolve resposta para o front
 header('Content-Type: application/json');
 echo json_encode($resposta);
 
+function verificaEmailExiste($email)
+{
 
+    global $conexao;
+
+    // Check connection
+    if (!$conexao) {
+        die("Não foi possível conectar: " . mysqli_connect_error());
+    }
+    $query = "select * from email where email = '$email'"; // where email = '"+$email+"'";
+    $select = mysqli_query($conexao, $query);
+
+
+    if ($select->num_rows > 0)
+        return true;
+    else
+        return false;
+
+    mysqli_close($conexao);
+}
 
 function cadastrarEmail($email)
 {
@@ -47,7 +74,7 @@ function cadastrarEmail($email)
 
     // Check connection
     if (!$conexao) {
-        die("Não foi possível conectar: " . mysqli_connect_error());        
+        die("Não foi possível conectar: " . mysqli_connect_error());
     }
     $query = "INSERT INTO email (email) VALUES ('" . $email . "')";
     $insert = mysqli_query($conexao, $query);
